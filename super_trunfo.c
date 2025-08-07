a705c1b4f3605b28e4e783733ff2f685d2fa3dd5
Arimatéa Sousa <arimatea.sousa@gmail.com>
2025-08-07 00:34:00 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,12 +1,74 @@
 #include <stdio.h> 
+#include <string.h>
 
-        int main(){
+    int main(){
+        char estado;
+        char codigo[5];
+        char nome[20];
+        int populacao;
+        float area;
+        double pib;
+        int numero;
 
+        //ENTRADA DE DADOS PELO USUÁRIO:
+        printf("\n");
+        printf("Carta 1:");
+        printf("\n");
 
+        printf("Digite a letra do estado: ");
+        scanf(" %c", &estado);
 
+        printf("Digite o código do estado: ");
+        scanf("%s", codigo);
 
+        getchar();
+        
+        printf("Digite o nome da Cidade: ");
+        fgets(nome, sizeof(nome), stdin);
+        nome[strcspn(nome,"\n")] = 0;
 
+        printf("Digite a população da cidade: ");
+        scanf("%d", &populacao);
 
+        printf("Qual a área da cidade? ");
+        scanf("%f", &area);
 
+        printf("Qual o PIB da cidade: ");
+        scanf("%lf", &pib);
 
-        }
+        printf("Qual o número de pontos turísticos? ");
+        scanf("%d", &numero);
+
+        //IMPRESSÃO DAS INFORMAÇÕES DO USUÁRIO:
+        printf("\n");
+        printf("Carta 01:");
+        printf("\n");
+
+        printf("01) Estado: %c;\n", estado);
+        printf("02) Código: %s;\n", codigo); 
+        printf("03) Nome da Cidade: %s;\n", nome);
+        printf("04) População: %d;\n", populacao);
+        printf("05) Área: %.2f;\n", area);
+        printf("06) PIB: %.2lF\n", pib);
+        printf("07) Número de Pontos Turísticos: %d;\n", numero);
+
+        printf("\n");
+
+/* getchar(); 
+
+    printf("Digite seu endereço: ");
+    fgets(endereco, 35, stdin);
+    endereco[strcspn(endereco, "\n")] = 0;
+    printf("Endereço: %s\n", endereco); */
+
+/* Carta 1:
+Estado: A
+Código: A01
+Nome da Cidade: São Paulo
+População: 12325000
+Área: 1521.11 km²
+PIB: 699.28 bilhões de reais
+Número de Pontos Turísticos: 50 */
+
+return 0;
+}
