b7fd3468171c30d6f62b9bd9bdce76b31935a884
Arimatéa Sousa <arimatea.sousa@gmail.com>
2025-08-28 08:31:11 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,117 +1,123 @@
 #include <stdio.h>
 
-    int main() {
+int main(){
 
-        // PRIMEIRA CARTA:
+// PRIMEIRA CARTA:
 
-        // VARIÁVEIS:
+printf("\n\n");
+printf(" >> DADOS CARTA 01:\n\n");
 
-        char estado1; // Apenas a letra do Estado de A a h;
-        char codigo1[5];  // Letra mais numero de 01 a 04;
-        char cidade1[20]; // Nome da cidade;
-        int populacao1;   // Habitantes
-        float area1; // Em km2;
-        float pib1; // Bilhoes de reais;
-        int pontos1; // Pontos turísticos.
+// VARIÁVEIS:
 
-        // LEITURA DOS DADOS DO USUÁRIO:
+char estado1;
+char codigo1[5];
+int populacao1;
+char cidade1[20];
+int turistico1;
+float area1, pib1, densidade1, pibpercapta1;
 
-        printf("\n");
-        printf("PRIMEIRA CARTA:\n");
-        printf("\n");
+printf("1. DIGITE O LETRA DO ESTADO: ");
+scanf(" %c", &estado1);
 
-        printf("Digite a letra do estado:\n");
-        scanf(" %c", &estado1);
+printf("2. DIGITE O CÓDIGO DA CIDADE: ");
+scanf("%s", codigo1);
 
-        printf("Digite o código da carta:\n");
-        scanf("%s", codigo1);
+printf("3. DIGITE O NOME DA CIDADE: ");
+scanf("%s", cidade1);
 
-        printf("Digite o nome da cidade:\n");
-        scanf("%s", cidade1);
+printf("4. QUAL A POPULAÇÃO? ");
+scanf("%d", &populacao1);
 
-        printf("Quantos habitantes tem a cidade?\n");
-        scanf("%d", &populacao1);
+printf("5. QUAL A ÁREA DA CIDADE (km2) ");
+scanf("%f", &area1);
 
-        printf("Qual a área da cidade?\n");
-        scanf("%f", &area1);
+printf("6. QUAL O PIB DA CIDADE? ");
+scanf("%f", &pib1);
 
-        printf("Qual o PIB da cidade:\n");
-        scanf("%f", &pib1);
+printf("7. QUANTOS PONTOS TURÍSTICOS TEM A CIDADE? ");
+scanf("%d", &turistico1);
 
-        printf("Quantos pontos turísticos tem a cidade?\n");
-        scanf("%d", &pontos1);
+// CÁLCULOS ARITMÉTICOS:
 
-        // SEGUNDA CARTA:
+densidade1 = populacao1 / area1;
+pibpercapta1 = (pib1 *1000000000) / populacao1;
 
-        // VARIÁVEIS:
+printf("\n\n");
+printf("** CARTA 01: **\n");
 
-        char estado2; 
-        char codigo2[5];  
-        char cidade2[20]; 
-        int populacao2;   
-        float area2; 
-        float pib2; 
-        int pontos2; 
 
-        // LEITURA DOS DADOS DO USUÁRIO:
+// IMPRIMIR NA TELA OS RESULTADOS - CARTA 01:
 
-        printf("\n");
-        printf("SEGUNDA CARTA:\n");
-        printf("\n");
+printf("1. Estado: %c;\n", estado1);
+printf("2. Código: %s;\n", codigo1);
+printf("3. Nome da Cidade: %s;\n", cidade1);
+printf("4. População: %d;\n", populacao1);
+printf("5. Área: %.2fkm2;\n", area1);
+printf("6. PIB: %.2F Bilhões de Reais;\n", pib1);
+printf("7. Número de Pontos Turísticos: %d;\n", turistico1);
+printf("8. Densidade Populacional: %.2f hab/km2;\n", densidade1);
+printf("9. PIB per Capta: %.2f reais;\n", pibpercapta1);
 
-        printf("Digite a letra do estado:\n");
-        scanf(" %c", &estado2);
+printf("\n\n");
 
-        printf("Digite o código da carta:\n");
-        scanf("%s", codigo2);
+// SEGUNDA CARTA:
 
-        printf("Digite o nome da cidade:\n");
-        scanf("%s", cidade2);
+printf("\n\n");
+printf("<< DADOS CARTA 02:\n\n");
 
-        printf("Quantos habitantes tem a cidade?\n");
-        scanf("%d", &populacao2);
+// VARIÁVEIS:
 
-        printf("Qual a área da cidade?\n");
-        scanf("%f", &area2);
+char estado;
+char codigo[5];
+int populacao;
+char cidade[20];
+int turistico;
+float area, pib, densidade, pibpercapta;
 
-        printf("Qual o PIB da cidade:\n");
-        scanf("%f", &pib2);
+printf("1. DIGITE O LETRA DO ESTADO: ");
+scanf(" %c", &estado);
 
-        printf("Quantos pontos turísticos tem a cidade?\n");
-        scanf("%d", &pontos2);
+printf("2. DIGITE O CÓDIGO DA CIDADE: ");
+scanf("%s", codigo);
 
-        // EXIBIÇÃO DAS INFORMAÇÕES NA TELA:
+printf("3. DIGITE O NOME DA CIDADE: ");
+scanf("%s", cidade);
 
-        // CARTA 01:
-        printf("\n");
-        printf("<< CARTA 01: >>\n");
-        printf("\n");
+printf("4. QUAL A POPULAÇÃO? ");
+scanf("%d", &populacao);
 
-        printf("1. Estado: %c;\n", estado1);
-        printf("2. Código: %s;\n", codigo1);
-        printf("3. Nome da Cidade: %s;\n", cidade1);
-        printf("4. População: %d Habitantes;\n", populacao1);
-        printf("5. Área: %.2f km²;\n", area1);
-        printf("6. PIB: %f Bilhões de reais;\n", pib1);
-        printf("7. Número de Pontos Turísticos: %d.\n", pontos1);
+printf("5. QUAL A ÁREA DA CIDADE (km2) ");
+scanf("%f", &area);
 
-        // CARTA 02:
-        printf("\n");
-        printf("<< CARTA 02: >>\n");
-        printf("\n");
+printf("6. QUAL O PIB DA CIDADE? ");
+scanf("%f", &pib);
 
-        printf("1. Estado: %c;\n", estado2);
-        printf("2. Código: %s;\n", codigo2);
-        printf("3. Nome da Cidade: %s;\n", cidade2);
-        printf("4. População: %d Habitantes;\n", populacao2);
-        printf("5. Área: %.2f km²;\n", area2);
-        printf("6. PIB: %f Bilhões de reais;\n", pib2);
-        printf("7. Número de Pontos Turísticos: %d.\n", pontos2);
+printf("7. QUANTOS PONTOS TURÍSTICOS TEM A CIDADE? ");
+scanf("%d", &turistico);
 
-        printf("\n");
-        printf("MARANATA - O Senhor Jesus vem!");
-        printf("\n\n");
-        
-        return 0;
+// CÁLCULOS ARITMÉTICOS:
 
-    }
+densidade = populacao / area;
+pibpercapta = (pib *1000000000) / populacao;
+
+printf("\n\n");
+printf("** CARTA 02: **\n");
+
+// IMPRIMIR NA TELA OS RESULTADOS:
+
+printf("1. Estado: %c;\n", estado);
+printf("2. Código: %s;\n", codigo);
+printf("3. Nome da Cidade: %s;\n", cidade);
+printf("4. População: %d;\n", populacao);
+printf("5. Área: %.2fkm2;\n", area);
+printf("6. PIB: %.2F Bilhões de Reais;\n", pib);
+printf("7. Número de Pontos Turísticos: %d;\n", turistico);
+printf("8. Densidade Populacional: %.2f hab/km2;\n", densidade);
+printf("9. PIB per Capta: %.2f reais;\n", pibpercapta);
+
+printf("\n\n");
+printf("MARANATA - O Senhor Jesus vem!\n\n");
+
+return 0;
+
+}
